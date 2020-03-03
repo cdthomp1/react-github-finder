7a5f04e1996e97b23be529e5fc5e9dde70a2f9dd
cdthomp1 <tho17015@byui.edu>
2020-03-02 21:18:32 -0700

    Base functionality done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
-import About from './components/pages/About'
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -12,7 +13,9 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
-    alert: null
+    alert: null,
+    user: {},
+    repos: []
   }
   /*   async componentDidMount() {
       this.setState({ loading: true });
@@ -23,12 +26,32 @@ class App extends Component {
 
   // Search Github users
   searchUsers = async (text) => {
-    this.setState({ loading: true });
+    this.setState({ loading: true, alert: null });
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.SECRET}`);
 
     this.setState({ users: res.data.items, loading: false })
   }
+
+  // Get a Single Github User
+
+  getUser = async (username) => {
+    this.setState({ loading: true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.SECRET}`);
+
+    this.setState({ user: res.data, loading: false })
+  }
+
+  //Get Users Repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.SECRET}`);
+
+    this.setState({ repos: res.data, loading: false })
+  }
+
 
   // Clear Users from State
   clearUsers = () => {
@@ -48,7 +71,7 @@ class App extends Component {
 
   render() {
 
-    const { users, loading } = this.state;
+    const { users, loading, user, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -64,6 +87,7 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path ='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (<User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />)}/>
             </Switch>
           </div>
         </div>
